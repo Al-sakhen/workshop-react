1b3fa35284859e9fdce194b876e4a2db672d2471
AmeenAbuAlnasser <ameenabu2016@gmail.com>
2022-11-23 12:05:47 +0200

    add page not found and style it in css and routing

--- src/App.js
+++ src/App.js
@@ -5,19 +5,21 @@ import Login from './components/Login';
 import Register from './components/Register';
 import Home from './components/Home';
 import Messages from './components/Messages';
-
+import { Route,Routes } from 'react-router-dom';
+import NotFound from './components/NotFound';
 function App() {
   return (
     <>
       <Navbar/>
-      <User/>
-      <Login/>
-      <hr />
-      <Register/>
-      <hr />
-      <Messages/>
-      <hr />
-      <Home/>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/register' element={<Register/>}></Route>
+        <Route path='/login' element={<Login/>}></Route>
+        <Route path='/user' element={ <User/>}></Route>
+        <Route path='/messages' element={<Messages/>}></Route>
+        <Route path='/*' element={<NotFound/>}></Route>
+      </Routes>
+      
     </>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,31 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom';
 export default function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg bg-custom navbar-dark bg-dark ">
             <div className="container">
-            <a className="navbar-brand" href="index.html"><img src="img/logo300.png" width={54} alt='' /> </a>
+            <Link className="navbar-brand" to=""><img src="img/logo300.png" width={54} alt='' /> </Link>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 Menu <span className="navbar-toggler-icon" />
             </button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav ml-auto d-none">
                 <li className="nav-item">
-                    <a className="nav-link" href="register.html">Register</a>
+                    <Link className="nav-link" to="register">Register</Link>
                 </li>
                 <li className="nav-item">
-                    <a className="nav-link" href="login.html">Login</a>
+                    <Link className="nav-link" to="login">Login</Link>
                 </li>
                 </ul>
                 <ul className="navbar-nav ml-auto ">
                 <li className="nav-item">
-                    <a className="nav-link" href="messages.html">Messages</a>
+                    <Link className="nav-link" to="messages">Messages</Link>
                 </li>
                 <li className="nav-item">
-                    <a className="nav-link" href>Settings</a>
+                    <a className="nav-link" href='*'>Settings</a>
                 </li>
                 <li className="nav-item">
-                    <a className="nav-link" href="index.html">Logout</a>
+                    <Link className="nav-link" to="login">Logout</Link>
                 </li>
                 </ul>
             </div>
